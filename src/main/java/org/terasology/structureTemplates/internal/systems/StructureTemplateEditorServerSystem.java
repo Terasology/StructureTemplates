df7f9e34d40672341fe2a377636e88f1a7f8ae01
Florian Köberle <florian@fkoeberle.de>
2017-01-23 21:55:30 +0100

    Handle a special case during block removal handling.

--- src/main/java/org/terasology/structureTemplates/internal/systems/StructureTemplateEditorServerSystem.java
+++ src/main/java/org/terasology/structureTemplates/internal/systems/StructureTemplateEditorServerSystem.java
@@ -175,7 +175,9 @@ public class StructureTemplateEditorServerSystem extends BaseComponentSystem {
 
         EntityRef editorEnitity = editsCopyRegionComponent.structureTemplateEditor;
         StructureTemplateEditorComponent editorComponent = editorEnitity.getComponent(StructureTemplateEditorComponent.class);
-
+        if (editorComponent == null) {
+            return; // can happen if entity got destroyed
+        }
         List<Region3i> originalRegions = editorComponent.absoluteRegionsWithTemplate;
         Set<Vector3i> positionsInTemplate = RegionMergeUtil.positionsOfRegions(originalRegions);
         if (!positionsInTemplate.contains(blockComponent.getPosition())) {
