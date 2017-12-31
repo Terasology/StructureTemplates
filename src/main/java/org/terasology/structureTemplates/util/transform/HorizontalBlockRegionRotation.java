ba4e606285c3bda0112c2da534757d9893506666
Alberto <alberto512c@gmail.com>
2017-12-31 19:27:58 +0100

    Improve code

--- src/main/java/org/terasology/structureTemplates/util/transform/HorizontalBlockRegionRotation.java
+++ src/main/java/org/terasology/structureTemplates/util/transform/HorizontalBlockRegionRotation.java
@@ -79,7 +79,7 @@ public class HorizontalBlockRegionRotation implements BlockRegionTransform {
             return sideDefinedBlockFamily.getBlockForSide(transformSide(block.getDirection()));
         } else if (blockFamily instanceof AttachedToSurfaceFamily) {
             // TODO add some proper method to block famility to not have to do this hack
-            return blockFamily.getBlockForPlacement(null, null, null, transformSide(block.getDirection()), null);
+            return blockFamily.getBlockForPlacement(null, null, null);
         }
         return block;
     }
