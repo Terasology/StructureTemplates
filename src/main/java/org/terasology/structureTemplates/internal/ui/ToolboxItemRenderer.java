6a94655bdeb78f0746d22d81753f6334d460f5d8
Michael Pollind <mpollind@gmail.com>
2020-12-12 21:39:51 -0800

    feat(JOML): migrate joml primatives 1.9.26     PR: https://github.com/MovingBlocks/Terasology/pull/4320

--- src/main/java/org/terasology/structureTemplates/internal/ui/ToolboxItemRenderer.java
+++ src/main/java/org/terasology/structureTemplates/internal/ui/ToolboxItemRenderer.java
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.structureTemplates.internal.ui;
 
-import org.joml.Rectanglei;
+import org.joml.primitives.Rectanglei;
 import org.terasology.math.JomlUtil;
 import org.joml.Vector2i;
 import org.terasology.nui.asset.font.Font;
